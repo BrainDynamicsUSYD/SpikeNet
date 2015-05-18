1a373ffe6f129682607bd523d95f481580bfafbe
Yifan Gu <yigu8115@uni.sydney.edu.au>
2015-05-18 17:08:40 +1000

    add one mat flie for simplified model
    Former-commit-id: 47673eab4b943000d5911b367b0032b48de18c1f

--- mean_fluctuation/compare_full_simple_models.m
+++ mean_fluctuation/compare_full_simple_models.m
@@ -1,50 +1,68 @@
 
-%%%%%%%%%%%% load data here
-% load pre-calculated table for (eq 3.4)
-table = load('v_CV_tables.mat');
-% manually fix some (possibly) numerical errors
-table.CV_mat(1:13,1) = 1;
-table.CV_mat(end-2:end,1) = 0;
-table.v_mat(end-2:end,1) = 0.5;
-% something is still wrong here!!!
-% transpose for interp2
-table.miu_V_mat = table.miu_V_mat';
-table.sigma_V_mat = table.sigma_V_mat';
-table.CV_mat = table.CV_mat';
-table.v_mat = table.v_mat';
-% The above table uses tau_m = 10 instead of 15!!!!!!!
-% Generate a new table???
-% Is tau_m's effect on the table linear??
+clc;clear all;close all;
+R = load('001-201505181500-15181_1431927741273_RYG.mat');
 
-% synI{}.I_mean
-% popV.V_mean{}
+
+% %%%%%%%%%%%%% compare V_mean time serious
+
 V_mean = R.popV.V_mean{1};
 V_std = R.popV.V_std{1};
-I_mean = R.synI{1}.I_mean;
-I_std = R.synI{1}.I_std;
 
-step_tot = R.step_tot;
-
-%%%%%%%%%%% compare I_mean as output?
-I_mean_s = zeros(1, step_tot); % s for simplified model
-I_std_s = zeros(1, step_tot);
-for t = 2:step_tot
-    mu_V_pre = V_mean(t-1);
-    sigma_V_pre =  sqrt(V_std(t-1));
-    [ out ] = get_eq3_4_table_lookup(mu_V_pre, sigma_V_pre, table,  'both');
-    I_mean_s(t) = out(1); % proportional to
-    I_std_s(t) = out(1)*out(2)^2; % proportional to
+I_mean = ones(size(V_mean))*1.5; % constant current
+I_std2 = zeros(size(V_mean));
+for i = 1:9
+    I_mean = I_mean + R.synI{i}.I_mean;
+    I_std2 = I_std2 + (R.synI{i}.I_std).^2; % this part is probably wrong!!!s
 end
 
 
-% compare V_mean time serious?
+% % subsampling
+% DT = 2;
+% I_mean = I_mean(1:DT:end);
+% V_mean = V_mean(1:DT:end);
+% V_std = V_std(1:DT:end);
+% I_std2 = I_std2(1:DT:end);
+
+
+xData = I_mean;
+yData = V_mean;
+zData = I_std2.^0.5;
+
+i_begin = 4.8*10^4;
+i_step = 2;
+i_end = 5.5*10^4;
+
+
+seg = i_begin:i_step:i_end;
+
+figure(1);
+set(gcf,'position', [680   349   663   726]);
+subplot(3,1,1);
+raster_plot_all(R,[500, zeros(1,8)])
+lh = line([1 1]*10^-4, [1 500], 'color', 'g');
+
+subplot(3,1,2:3);
+xlim(minmax(xData(seg)));
+ylim(minmax(yData(seg)));
+zlim(minmax(zData(seg)));
+
+hold on;
+;
+i_pre = i_begin;
+for i = seg
+    
+    if mod(i, 100) == 0
+    subplot(3,1,1);
+    set(lh,'XData', [i i]*10^-4);
+    end
+    
+    subplot(3,1,2:3);
+    plot3([xData(i_pre) xData(i)], [yData(i_pre), yData(i)], [zData(i_pre) zData(i)], '-');
+    title(sprintf('%.1f ms', i*0.1))
+    pause(0.001);
+    
+    i_pre = i;
+end
 
 
 
-
-
-
-
-
-
-% solve the mean and CV of first passage time of the O-U process, i.e, (eq 3.4)
