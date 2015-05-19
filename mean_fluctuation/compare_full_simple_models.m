e6eda001375dcd996619de0727d78d32f8d7546f
Yifan Gu <yigu8115@uni.sydney.edu.au>
2015-05-19 17:16:35 +1000

    updated phase flow visualization file
    Former-commit-id: 94bda53ce5809d4a47104bb8b746e1ee6ab707cc

--- mean_fluctuation/compare_full_simple_models.m
+++ mean_fluctuation/compare_full_simple_models.m
@@ -1,72 +1,83 @@
 
 clc;clear all;close all;
-R = load('001-201505191310-17128_1432012658497_RYG.mat');
 
-
-
+R=  load('012-201505191310-17121_1432011996519_RYG.mat');
 % %%%%%%%%%%%%% compare V_mean time serious
 
 V_mean = R.pop_stats.V_mean{1};
 V_std = R.pop_stats.V_std{1};
-I_mean = R.pop_stats.I_input_mean{1}; % constant current
+I_mean = R.pop_stats.I_input_mean{1} + 1.5; % 1.5 is the constant current
 I_std = R.pop_stats.I_input_std{1};
+num_spikes = R.num_spikes{1};
 
-
-% I_std_comp = zeros(size(I_std));
-% for i = 1:9
-%     I_std_comp = I_std_comp + (R.syn_stats{i}.I_std(5001:end)).^2;
-% end
-% I_std_comp = I_std_comp.^0.5;
-% % the above is not always a good assumption
-
-
-% % subsampling
-% DT = 2;
-% I_mean = I_mean(1:DT:end);
-% V_mean = V_mean(1:DT:end);
-% V_std = V_std(1:DT:end);
-% I_std2 = I_std2(1:DT:end);
-
+sigma = 10/0.1;
+size = 5*sigma;
+x = linspace(-size / 2, size / 2, size);
+gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+gaussFilter = gaussFilter / sum (gaussFilter); %
+num_spikes_sm = transpose(filter(gaussFilter,1, num_spikes'));
 
 xData = I_mean;
 yData = V_mean;
 zData = I_std;
+cData = num_spikes_sm;
 
-i_begin = 4.8*10^4;
-i_step = 2;
-i_end = 5.5*10^4;
 
+
+%%%
+clear I_mean V_meam I_std;
+
+
+i_begin = 5*10^4;
+i_step = 10;
+i_end = 8*10^4;
 
 seg = i_begin:i_step:i_end;
 
+
 figure(1);
 set(gcf,'position', [680   349   663   726]);
+
 subplot(3,1,1);
-raster_plot_all(R,[500, zeros(1,8)])
-lh = line([1 1]*10^-4, [1 500], 'color', 'g');
+%raster_plot_all(R,[500, zeros(1,8)])
+plot( (1:40*10^4)*10^-4, cData(1:40*10^4) );
+lh = line([1 1]*10^-4, [0  1], 'color', 'g');
+
+clear R;
 
 subplot(3,1,2:3);
+grid on;
+
+
 xlim(minmax(xData(seg)));
 ylim(minmax(yData(seg)));
 zlim(minmax(zData(seg)));
 
-hold on;
+cmin = min(cData(seg))-1;
+cmax = max(cData(seg));
+crange = cmax - cmin;
+creso = 1000;
+cmap = cool(creso);
 
+hold on;
 i_pre = i_begin;
 for i = seg
     
-    if mod(i, 100) == 0
-    subplot(3,1,1);
-    set(lh,'XData', [i i]*10^-4);
-    end
+%     if mod(i, 2000) == 0
+%         cla
+%     end
+%     if mod(i, 1000) == 0
+%     subplot(3,1,1);
+%     set(lh,'XData', [i i]*10^-4);
+%     end
     
     subplot(3,1,2:3);
-    plot3([xData(i_pre) xData(i)], [yData(i_pre), yData(i)], [zData(i_pre) zData(i)], '-');
+    plot3([xData(i_pre) xData(i)], [yData(i_pre), yData(i)], [zData(i_pre) zData(i)],...
+        '-','color',  cmap( ceil((cData(i)-cmin)/crange*creso), :) );
     title(sprintf('%.1f ms', i*0.1))
-    pause(0.001);
+    % pause(0.001);
     
     i_pre = i;
 end
-
 
 
