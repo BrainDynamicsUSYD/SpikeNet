3564e110314c24bb1498670845744aa05f986e62
Yifan Gu <yigu8115@gmail.com>
2016-02-18 12:14:10 +1100

    add a function that generates correlated lognormal random variables
    Former-commit-id: cf8ab84291a57e00b489644052241503ebf87eb7

---
+++ matlab_interface/my_logn_rand.m
@@ -0,0 +1,38 @@
+function [ X ] = my_logn_rand( Mu, Sigma, CorrMat_log, N, mu_sigma_mode )
+%[ X ] = my_logn_rand( Mu, Sigma, CorrMat_log, N, mu_sigma_mode )
+%   'mu_sigma_mode' specifies whether the input Mu and Sigma are for the
+%   pre-exponentiated normal distribution ('mu_sigma_norm') or for the
+%   post-exponentiated lognormal distritbution ('mu_sigma_norm')
+%
+%
+%   Code tested.
+
+%   For more information see: Aggregration of Correlated Risk Portfolios:
+%   Models and Algorithms; Shaun S. Wang, Phd.  Casualty Actuarial Society
+%   Proceedings Volume LXXXV www.casact.org
+%
+%   Author: Stephen Lienhard
+
+switch lower(mu_sigma_mode)
+    case 'mu_sigma_log'
+        Mu_norm = log((Mu.^2)./sqrt(Sigma.^2+Mu.^2));
+        Sigma_norm = sqrt(log(Sigma.^2./(Mu.^2)+1));
+    case 'mu_sigma_norm'
+        Mu_norm = Mu;
+        Sigma_norm = Sigma;
+end
+% The inverse transformation of the above one is lognstat
+% [Mu_log, Sigma_log] = [Mu_norm, Sigma_norm]
+
+% Calculate the covariance structure
+sigma_down = repmat( Sigma_norm(:)' , numel(Sigma_norm(:)), 1                    );
+sigma_acrs = repmat( Sigma_norm(:)  , 1                   , numel(Sigma_norm(:)) );
+covv = log( CorrMat_log .* sqrt(exp(sigma_down.^2)-1) .* ...
+                       sqrt(exp(sigma_acrs.^2)-1) + 1 );
+
+% The Simulation
+X = exp( mvnrnd( Mu_norm(:) , covv , N ));
+
+
+end
+
