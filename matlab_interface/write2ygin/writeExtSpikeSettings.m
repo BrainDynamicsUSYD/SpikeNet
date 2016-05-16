d6a9c5d679702f85cecceec25888c3cc61696257
Yifan Gu <yigu8115@gmail.com>
2016-05-16 11:02:33 +1000

    fix an important bug where the matlab interface is not consistent with INIT005 protocol
    Former-commit-id: 568ec80059caa610377cb0870a07dc06a2e27e78

--- matlab_interface/write2ygin/writeExtSpikeSettings.m
+++ matlab_interface/write2ygin/writeExtSpikeSettings.m
@@ -1,11 +1,11 @@
-function writeExtSpikeSettings(FID, pop_ind, type_ext, K_ext,  Num_ext, rate_ext, ia, ib)
+function writeExtSpikeSettings(FID, pop_ind, type_ext, K_ext,  Num_ext, rate_ext_t, ia, ib)
 % write external spike settings
 %      FID: file id for writing data
 %  pop_ind: index of neuron population to receive external spikes
 % type_ext: type of external chemical connection (1:AMPA, 2:GABA, 3:NMDA)
 %    K_ext: strength for external chemical connection
 %  Num_ext: number of external neurons connected to each neuron in pop_ind
-% rate_ext: spiking rate for each external neurons
+% rate_ext_t: spiking rate for each time step
 %       ia: start neuron index
 %       i:  end neuron index
 % Note that each external neuron is independent Poissonian neuron
@@ -13,10 +13,6 @@ function writeExtSpikeSettings(FID, pop_ind, type_ext, K_ext,  Num_ext, rate_ext
 pop_ind = pop_ind - 1;
 type_ext = type_ext - 1;
 
-if nargin == 6
-    ia = 1;
-    ib = length(rate_ext);
-end
 
 ia = ia - 1;
 ib = ib - 1;
@@ -25,6 +21,6 @@ ib = ib - 1;
 % fprintf(FID, '%s\n', '# external spikes // (pop_ind, type_ext, K_ext:miuSiemens,  Num_ext, ia, ib;  rate_ext(t):Hz)');
 fprintf(FID, '%s\n', '> INIT005');
 fprintf(FID, '%.9f,', [pop_ind, type_ext, K_ext,  Num_ext, ia, ib]); fprintf(FID,'\n');
-fprintf(FID, '%.9f,', rate_ext); fprintf(FID,'\n\n');
+fprintf(FID, '%.9f,', rate_ext_t); fprintf(FID,'\n\n');
 end
 
