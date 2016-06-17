8009ddd5f65132ebf42de34b3cd5c145d2e05c30
Yifan Gu <yigu8115@gmail.com>
2016-06-17 12:29:55 +1000

    Add Hilbert transformation into SWR analysis
    Former-commit-id: 9a72a98eaf29093ae79df833b89d1a545d43c9e1

--- matlab_interface/get_SWR.m
+++ matlab_interface/get_SWR.m
@@ -1,10 +1,13 @@
 function [ R ] = get_SWR( R )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+% ref: Hippocampal place-cell sequences depict future paths to remembered goals.
 
 LFP = R.LFP{1};
 dt = R.dt;
 fs = 1/(dt*1e-3); % sampling frequency (Hz)
+width = 12.5; %ms
+[ Kernel ] = spike_train_kernel_YG( width, dt, 'gaussian_unit' );
 
 % Butterworth filter
 order = 4; % 4th order
@@ -12,20 +15,30 @@ lowFreq = 10; % broad band (10?250 Hz)
 hiFreq = 250;
 Wn = [lowFreq hiFreq]/(fs/2);
 [b,a] = butter(order/2,Wn,'bandpass'); %The resulting bandpass and bandstop designs are of order 2n.
-
 LFP_broad = filter(b,a,LFP);
+% hilbert transformation
+LFP_broad = abs(hilbert(LFP_broad));
+% gaussian smoothing
+LFP_broad = conv( LFP_broad, Kernel,'same');
 
 % Butterworth filter
-
 order = 4; % 4th order
-lowFreq = 80; % ripple band (80?180 Hz)
-hiFreq = 180;
+lowFreq = 150; % ripple band (80?180 Hz)
+hiFreq = 250;
 Wn = [lowFreq hiFreq]/(fs/2);
 [b,a] = butter(order/2,Wn,'bandpass'); %The resulting bandpass and bandstop designs are of order 2n.
 
-LFP_ripple = filter(b,a,LFP_broad);
+LFP_ripple = filter(b,a,LFP);
+% hilbert transformation
+LFP_ripple = abs(hilbert(LFP_ripple));
+% gaussian smoothing
+LFP_ripple = conv( LFP_ripple, Kernel,'same');
+
 
 R.SWR.LFP_broad = LFP_broad;
 R.SWR.LFP_ripple = LFP_ripple;
+R.SWR.lowFreq = lowFreq;
+R.SWR.hiFreq = hiFreq;
+R.SWR.gauss_width = width;
 end
 
