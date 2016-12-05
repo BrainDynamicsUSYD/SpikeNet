49de08baca6118ec6fcee184c622ef1cb5050f53
Yifan Gu <yigu8115@gmail.com>
2016-12-05 13:30:07 +1000

    Add a new case file and other minior changes
    Former-commit-id: bff34a15766f965cc8b51f9a3f167fef96acd91f

--- matlab_interface/visualisation_tools/plot_SWR_multi.m
+++ matlab_interface/visualisation_tools/plot_SWR_multi.m
@@ -1,126 +1,104 @@
-function plot_SWR_multi( R, save_figure, no_given, seg_given )
+function peak_check = plot_SWR_multi( R, LFP_grid, I,J,seg_ind )
 %   -1 for no figure, 0 for displaying figure, 1 for saving figure
 %
+
 %   If the firing history is too long, data will be segmented into several
 %   history fractions and plotted separately.
 disp('plot_SWR_multi...');
 
 
-% if nargin == 0
-%     Result_cell = CollectRYG();
-% end
-if nargin <= 1
-    save_figure = 1; % default
-end
-if save_figure == 1
-    figure_visibility = 'off'; % 'on', 'off'
-else
-    figure_visibility = 'on';
-end
-
 dt = R.dt;
-step_tot = R.step_tot;
+fs = 1/(dt*1e-3); % sampling frequency (Hz)
 
 
 
-seg_size = 0.5*10^4; % 2*10^4 for 2-pop, segmentation size for each plot
-seg_num = ceil(step_tot/seg_size);
-[no, ~] = size(R.LFP.LFP_broad);
-nos = 1:no;
 
-if nargin >= 3
-    nos = no_given;
-end
+figure_width = 15; % cm
+figure_hight = 20.0; %cm
 
-if nargin == 4
-    seg_num = 1;
-end
-for seg = 1:seg_num
+h_SWR = figure('NumberTitle','off','name','SWR-multi' ,'color','w', ...
+    'units', 'centimeters', 'position', [3, 2, 3+figure_width, 2+figure_hight], ...
+    'PaperSize', [figure_width, figure_hight] ,'PaperPositionMode','auto');
+
+ax = [];
+ax_2 = [];
+c_min = inf;
+c_max = -inf;
+nos = length(I);
+peak_check= [];
+for ii = 1:nos
     
-    ii = 0;
-    
-    figure_width = 8; % cm
-    figure_hight = 20.0; %cm
-    
-    h_SWR = figure('NumberTitle','off','name','SWR-multi' ,'color','w', ...
-        'units', 'centimeters', 'position', [3, 2, 3+figure_width, 2+figure_hight], ...
-        'PaperSize', [figure_width, figure_hight] ,'PaperPositionMode','auto');
-    
-    ax = [];
-    c_min = inf;
-    c_max = -inf;
-    for i = nos
-        ii = ii + 1;
-        ax_tmp = subaxis(no,1,ii,'SV',0.01);
-        if ii ~= length(nos)
+    ax_tmp = subaxis(nos,2,2*(ii-1)+1,'SV',0.01);
+    if ii ~= nos
         axis off;
-        end
-        ax = [ax ax_tmp];
-        hold on;
-        
-        seg_ind = get_seg(step_tot, seg_size, seg);
-        if nargin == 4
-            seg_ind = seg_given;
-        end
-        
-        
-        
-        t = (seg_ind-1)*dt*1e-3; % second
-        %
-        
-        
-        %         rip_tmp = R.LFP.LFP_ripple(i,seg_ind);
-        %         plot(t,  rip_tmp, 'w');
-        
-        
-        %         rms_tmp = R.LFP.LFP_ripple_rms(i,seg_ind);
-        %         rms_scaled = rms_tmp/max(rms_tmp)*max(rip_tmp);
-        %         plot(t, rms_scaled,'g'); %ylabel('Ripple Hilbert')
-        
-        %         hil_tmp = R.LFP.LFP_ripple_hilbert(i,seg_ind);
-        %         hil_scaled = hil_tmp/max(hil_tmp)*max(rip_tmp);
-        %         plot(t, hil_scaled,'r'); %ylabel('Ripple Hilbert')
-        %
-        %         plot(t, ones(size(t))*R.LFP.ripple_event.hil_mean_baseline(i,end)/max(hil_tmp)*max(rip_tmp) );
-        %         plot(t, ones(size(t))*(R.LFP.ripple_event.hil_mean_baseline(i,end) + R.LFP.ripple_event.no_std*R.LFP.ripple_event.hil_std_baseline(i,end))/max(hil_tmp)*max(rip_tmp) );
-        %
-        
-        %
-        scales = R.LFP.wavelet.scales;
-        x_tmp = R.LFP.LFP_ripple(i,seg_ind);
-        coeffs_tmp = abs(cwt(x_tmp,scales,'cmor1.5-1'))';
-        c_min = min(c_min, min(coeffs_tmp(:)));
-        c_max = max(c_max, max(coeffs_tmp(:)));
-        
-        freqrange = [R.LFP.lowFreq R.LFP.hiFreq];
-        imagesc('XData',t,'YData',R.LFP.wavelet.pseudoFreq,'CData',transpose(coeffs_tmp));
-        ylim(freqrange)
-        
-        xlim(minmax(t));
-        
-        
-    end
-    for iii = 1:ii
-        caxis(ax(iii), [c_min c_max]);
-    end
-    
-    
-    % save figure
-    if save_figure == 1
-        fprintf('\t Saving figure...');
-        print(h_SWR, '-dpdf', strcat( R.stamp, '_SWR_',sprintf('%02g_', i), sprintf('%02g', seg)));
-        % delete(h_SWR);
-        close gcf; clear gcf;
-        fprintf('done.\n');
     else
-        next = input('\t Next figure?');
-        delete(gcf);
+        set(gca, 'box','off','XTickLabel',[],'XTick',[],'YTickLabel',[],'YTick',[])
     end
     
+    ax = [ax ax_tmp];
+    hold on;
+    
+    
+    
+    
+    t = (0:(range(seg_ind)))*dt; % msec
+    
+    scales = R.LFP.wavelet.scales;
+    LFP_tmp = LFP_grid(I(ii),J(ii),:);
+    LFP_tmp = LFP_tmp(:)';
+    coeffs_tmp = abs(cwt(LFP_tmp,scales,'cmor1.5-1'))';
+    coeffs_tmp =  coeffs_tmp(seg_ind,:);
+    c_min = min(c_min, min(coeffs_tmp(:)));
+    c_max = max(c_max, max(coeffs_tmp(:)));
+    
+    freqrange = [R.LFP.lowFreq R.LFP.hiFreq];
+    imagesc('XData',t,'YData',R.LFP.wavelet.pseudoFreq,'CData',transpose(coeffs_tmp));
+    ylim(freqrange)
+    
+    xlim(minmax(t));
+    
+
+        
+
+   
+    % Butterworth filter
+    order = 4; % 4th order
+    lowFreq = 100; % ripple band (default values for this function are 150-250 Hz)
+    hiFreq = 250;
+    Wn = [lowFreq hiFreq]/(fs/2);
+    [b,a] = butter(order/2,Wn,'bandpass'); %The resulting bandpass and bandstop designs are of order 2n.
+    gaus_width = 5; %ms
+    
+    LFP_ripple = filter(b,a,  LFP_tmp);
+    
+    xlim(minmax(t));
+    
+    [ Kernel ] = spike_train_kernel_YG( gaus_width, R.dt, 'gaussian_unit' );
+    
+    hil_tmp = abs(hilbert( LFP_ripple));
+    ripple_power = conv(hil_tmp, Kernel,'same');
+    
+    ax1 = gca;
+    Position = get(ax1,'Position');
+    ax2tmp = axes('Position',Position);
+    ax_2 = [ax_2  ax2tmp];
+    set(ax2tmp, 'visible', 'off')
+    
+    hold on;
+    plot(t,  ripple_power(seg_ind), 'Parent',ax2tmp,'Color','w','LineWidth',1)
+    plot(t, LFP_ripple(seg_ind), 'Parent',ax2tmp,'Color','w','LineWidth',1)
+    peak_check = [peak_check max(ripple_power(seg_ind))];
+   
+end
+for iii = 1:ii
+    caxis(ax(iii), [c_min c_max]);
+end
+
+linkaxes(ax_2, 'x');
+
 end
 
 
-end
 
 
 
--- matlab_interface/visualisation_tools/show_LFP_continous.m
+++ matlab_interface/visualisation_tools/show_LFP_continous.m
@@ -2,74 +2,119 @@ function [peak] = show_LFP_continous(R, show)
 dt = R.dt;
 stamp = R.stamp;
 samp_file = [stamp(1:end-3) '0_neurosamp'];
-load(samp_file);
 
-[N, steps] = size(I_AMPA);
-if N ~= R.N(1)
-    error('Not all of the excitatory neurons are sampled!')
-end
-w = sqrt(N);
 
-hw = (w-1)/2;
-[Lattice, ~] = lattice_nD(2, hw);
-
-if ~exist('ripple_power_grid','var')
-    dist = lattice_nD_find_dist(Lattice, hw,  0, 0);
-    LFP_range_sigma = R.ExplVar.LFP_range_sigma;
-    gaus_tmp = 1/(LFP_range_sigma*sqrt(2*pi))*exp(-0.5*(dist/LFP_range_sigma).^2) .* double(dist <= LFP_range_sigma*2.5);
-    m = reshape(gaus_tmp, w, w);
-    LFP_grid = zeros(w,w,steps);
-    for i = 1:steps
-        x = reshape(abs(I_AMPA(:,i)) + abs(I_GABA(:,i)), w, w);
-        y = convolve2(x, m, 'wrap');
-        
-        LFP_grid(:,:,i) = y;
-        %i
-    end
-    clear I_AMPA I_GABA;
-    %%%% Ripple power
-    fs = 1/(R.dt*1e-3); % sampling frequency (Hz)
-    % Butterworth filter
-    order = 4; % 4th order
-    lowFreq = 100; % ripple band (default values for this function are 150-250 Hz)
-    hiFreq = 250;
-    Wn = [ lowFreq  hiFreq]/(fs/2);
-    [b,a] = butter(order/2,Wn,'bandpass'); %The resulting bandpass and bandstop designs are of order 2n.
-    gaus_width = 5; %ms
-    [ Kernel ] = spike_train_kernel_YG( gaus_width, R.dt, 'gaussian_unit' );
-    ripple_power_grid = zeros(size(LFP_grid));
-    for i = 1:w
-        for j= 1:w
-            ripple_tmp = filter(b,a,LFP_grid(i,j,:));
-            ripple_tmp = ripple_tmp(:);
-            hil_tmp = abs(hilbert( ripple_tmp));
-            ripple_power_grid(i,j,:) = conv(hil_tmp, Kernel,'same');
-        end
-        %w*(i-1)+j
-    end
-    save(samp_file, 'ripple_power_grid', '-append')
-end
-
-if ~exist('peak','var')
-    peak = [];
-    for i = 1:steps
-        A =  ripple_power_grid(:,:,i);
-        [peak_mag,I] = max(A(:));
-        [I_row, I_col] = ind2sub(size(A),I);
-        peak = [peak; I_row, I_col, peak_mag];
-    end
+try
+    load(samp_file, 'peak');
+    figure('NumberTitle','off','Name','Peak','color', 'w');
+    xlim([0 63]);
+    ylim([0 63]);
+    box on;
+    hold on;
     
-    save(samp_file,  'peak', '-append')
-end
-figure('NumberTitle','off','Name','Peak','color', 'w');
-multicolor_line(peak(:,1), peak(:,2), peak(:,3), 2)
+    peak_mag = peak(:,3);
+    peak_thre =  quantile(peak_mag,0.80);
+    peak_seq = peak_mag > peak_thre;
+    peak_min_steps = round(30/dt); % 30 ms
+    peak_seq  = persistence_requirement( peak_seq , peak_min_steps );
+    cutoff = 1;
+    [~, peak_du, ~, peak_start, ~] = seq_postprocess(peak_seq, 1, cutoff);
 
-X = ripple_power_grid;
+    for ii = 1:length(peak_du)
+        a = peak_start(ii);
+        b = a + peak_du(ii) - 1;
+        multicolor_line(peak(a:b,1), peak(a:b,2), peak(a:b,3), 2);
+        a,b
+        pause();
+        
+    end
+catch
+end
 
 if nargin == 1;
     show = 1;
 end
+
 if show == 1
+    try
+        load(samp_file, 'ripple_power_grid');
+        [N_s, ~, steps] = size(ripple_power_grid);
+        N = N_s^2;
+        if N ~= R.N(1)
+            error('Not all of the excitatory neurons are sampled!')
+        end
+        w = sqrt(N);
+        hw = (w-1)/2;
+        [Lattice, ~] = lattice_nD(2, hw);
+        
+    catch
+    end
+    if ~exist('ripple_power_grid','var')
+        load(samp_file);
+        
+        
+        [N, steps] = size(I_AMPA);
+        if N ~= R.N(1)
+            error('Not all of the excitatory neurons are sampled!')
+        end
+        w = sqrt(N);
+        hw = (w-1)/2;
+        [Lattice, ~] = lattice_nD(2, hw);
+        
+        dist = lattice_nD_find_dist(Lattice, hw,  0, 0);
+        LFP_range_sigma = R.ExplVar.LFP_range_sigma;
+        gaus_tmp = 1/(LFP_range_sigma*sqrt(2*pi))*exp(-0.5*(dist/LFP_range_sigma).^2) .* double(dist <= LFP_range_sigma*2.5);
+        m = reshape(gaus_tmp, w, w);
+        LFP_grid = zeros(w,w,steps);
+        for i = 1:steps
+            x = reshape(abs(I_AMPA(:,i)) + abs(I_GABA(:,i)), w, w);
+            y = convolve2(x, m, 'wrap');
+            
+            LFP_grid(:,:,i) = y;
+            %i
+        end
+        clear I_AMPA I_GABA;
+        %%%% Ripple power
+        fs = 1/(R.dt*1e-3); % sampling frequency (Hz)
+        % Butterworth filter
+        order = 4; % 4th order
+        lowFreq = 100; % ripple band (default values for this function are 150-250 Hz)
+        hiFreq = 250;
+        Wn = [ lowFreq  hiFreq]/(fs/2);
+        [b,a] = butter(order/2,Wn,'bandpass'); %The resulting bandpass and bandstop designs are of order 2n.
+        gaus_width = 5; %ms
+        [ Kernel ] = spike_train_kernel_YG( gaus_width, R.dt, 'gaussian_unit' );
+        ripple_power_grid = zeros(size(LFP_grid));
+        for i = 1:w
+            for j= 1:w
+                ripple_tmp = filter(b,a,LFP_grid(i,j,:));
+                ripple_tmp = ripple_tmp(:);
+                hil_tmp = abs(hilbert( ripple_tmp));
+                ripple_power_grid(i,j,:) = conv(hil_tmp, Kernel,'same');
+            end
+            %w*(i-1)+j
+        end
+        save(samp_file, 'ripple_power_grid', 'LFP_grid', '-append')
+    end
+    
+    if ~exist('peak','var')
+        peak = [];
+        for i = 1:steps
+            A =  ripple_power_grid(:,:,i);
+            [peak_mag,I] = max(A(:));
+            [I_row, I_col] = ind2sub(size(A),I);
+            peak = [peak; I_row, I_col, peak_mag];
+        end
+        
+        save(samp_file,  'peak', '-append')
+    end
+    
+    
+    
+    X = ripple_power_grid;
+    clear ripple_power_grid;
+    
+    
     figure('NumberTitle','off','Name','Ripple Hilbert Power','color', 'w');
     mm = minmax(reshape(X(:),1,[]));
     xlim([-hw hw]);
