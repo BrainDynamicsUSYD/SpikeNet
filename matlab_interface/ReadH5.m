69034d6fcdf577c3462e9d6b19658c0574fd4518
Yifan Gu <yigu8115@gmail.com>
2017-02-05 14:52:44 +1000

    Add the function to read synaptic connection definitions in h5 files into matlab
    Former-commit-id: f279ecc4282125fd2f44827b0506c1cf7470f74f

--- matlab_interface/ReadH5.m
+++ matlab_interface/ReadH5.m
@@ -342,19 +342,6 @@ end
 end % if ~isempty(name)
 
 
-function r = try_h5read(file, dataset_name)
-r = [];
-try
-    r = h5read(file, dataset_name);
-catch ME
-    switch ME.identifier
-        case 'MATLAB:imagesci:h5read:libraryError'
-        otherwise
-            rethrow(ME)
-    end
-    
-end
-end
 
 
 % function Data = ReformatVI_sample(Data)
