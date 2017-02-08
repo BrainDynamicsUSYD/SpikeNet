6281ee87c65ea653f437246a90e51fce9da4269c
Yifan Gu <yigu8115@gmail.com>
2017-02-08 17:55:23 +1000

    Small improvements
    Former-commit-id: 834b85aab7901a1921dfbefcb5b33857938ec0f9

--- matlab_interface/visualisation_tools/slim_colorbar.m
+++ matlab_interface/visualisation_tools/slim_colorbar.m
@@ -1,16 +1,23 @@
-function [ ax_c ] = slim_colorbar(ax_acc, width)
-%SLIM_COLORBAR Display color bar (color scale)
+function [ ax_c ] = slim_colorbar(varargin)
+% SLIM_COLORBAR Display color bar (color scale)
 % Just like matlab colorbar but it keeps the size of the associated axes
 % unchanged and allows user to specify the width of colorbar.
-%
-% ARGUMENTS:
-%        ax_acc -- the associated axes
+% 
+% USAGE:
+%  slim_colorbar
+%  slim_colorbar(width)
+%  slim_colorbar(width, ax_acc)
+%  [ ax_c ] = slim_colorbar(...)
+% 
+% ARGUMENTS (optional):
 %        width -- width of colorbar (% of figure width, suggest 0.01)
-%
+%        ax_acc -- the associated axes (the default is the current one)
+% 
 % OUTPUT: 
 %        ax_c -- axes handle of the colorbar
-% USAGE:
-%{
+% 
+% EXAMPLE:
+% {
 %     figure('NumberTitle','Off','Name','Slim colorbar','color','w');
 %     subplot(2,2,1);
 %     plot(1:10);
@@ -23,12 +30,24 @@ function [ ax_c ] = slim_colorbar(ax_acc, width)
 %     ax_acc = subplot(2,2,4);
 %     plot(1:10);
 %     xlabel('Slim colorbar')
-%     slim_colorbar(ax_acc, 0.01)
-%}
+%     slim_colorbar(0.01, ax_acc); 
+%     % or slim_colorbar(0.01)
+%     % or simply slim_colorbar;
+% }
 %
-% Yifan Gu, Sep 2016
+% Yifan Gu, Feb 2017
 % yigu8115@gmail.com
 
+width = 0.01;
+ax_acc = gca;
+
+if nargin >= 1
+    width = varargin{1};
+end
+if nargin == 2
+    ax_acc = varargin{2};
+end
+    
 ax_acc_size = get(ax_acc, 'position');
 ax_c = colorbar('peer',ax_acc);
 set(ax_acc, 'Position', ax_acc_size);pause(1)
