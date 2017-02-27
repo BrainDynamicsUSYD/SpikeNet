38d8087c676ffb2965b705ec0f3eb6fab1824735
Yifan Gu <yigu8115@gmail.com>
2017-02-27 16:17:31 +1000

    Now all the data needed for structural variance analysis can be collected
    Former-commit-id: 87a5fb753bfc304c6ba25ba8ac9d5d71fa5ad9b4

--- matlab_interface/post_analysis/Welford_online.m
+++ matlab_interface/post_analysis/Welford_online.m
@@ -1,4 +1,5 @@
 function [new_mean, new_var] = Welford_online(new_data, old_mean, old_var, step_now, is_end)
+
 % online mean and var calculation: Welford's method (1962, Technometrixcs)
 new_mean = old_mean + (new_data - old_mean) ./ step_now;
 new_var = old_var + (new_data - old_mean) .* (new_data - old_mean);
