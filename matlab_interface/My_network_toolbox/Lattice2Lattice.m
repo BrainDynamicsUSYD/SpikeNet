15bd5766f789f1ffe8edecc5bf65767bd7693924
Yifan Gu <yigu8115@gmail.com>
2017-03-20 17:42:25 +1000

    Add distance cutoff
    Former-commit-id: c56544b94fb9d0590a07e549432835d3cc12907b

--- matlab_interface/My_network_toolbox/Lattice2Lattice.m
+++ matlab_interface/My_network_toolbox/Lattice2Lattice.m
@@ -1,4 +1,4 @@
-function [ I,J ] = Lattice2Lattice( L_pre, L_post, hw, tau_c, p0 )
+function [ I,J ] = Lattice2Lattice( L_pre, L_post, hw, tau_c, p0, dist_cutoff )
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -8,8 +8,13 @@ N_post = length(L_post(:,1));
 I = [];
 J = [];
 for i = 1:N_pre
+
     dist = lattice_nD_find_dist(L_post, hw, L_pre(i,1), L_pre(i,2) );
     dist_factor = exp(-dist/tau_c);
+    % distance cutoff
+    if nargin == 6
+         dist_factor(dist > dist_cutoff) = NaN;
+    end
     
     [~, ind] = sort( rand(N_post,1)./dist_factor, 'ascend' );
     chosen_j = ind(1:poissrnd(N_post*p0));
