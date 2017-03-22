291bc4c580941feb47c2fdccd885b1546b922b07
Yifan Gu <yigu8115@gmail.com>
2017-03-22 16:55:46 +1000

    Update the rich club measure function to its lastest version
    Former-commit-id: b55a1723820db3c573ce618117e89915be8b9912

--- matlab_interface/My_network_toolbox/Brain Connection Toolbox/Measurements/rich_club_wd.m
+++ matlab_interface/My_network_toolbox/Brain Connection Toolbox/Measurements/rich_club_wd.m
@@ -1,36 +1,41 @@
 function   [Rw] = rich_club_wd(CIJ,varargin)
+%RICH_CLUB_WD	Rich club coefficients curve (weighted directed graph)
+%
+%   Rw = rich_club_wd(CIJ,varargin)
+%
+%   The weighted rich club coefficient, Rw, at level k is the fraction of
+%   edge weights that connect nodes of degree k or higher out of the
+%   maximum edge weights that such nodes might share.
+%
+%   Inputs:
+%       CIJ:        weighted directed connection matrix
+%
+%       k-level:    (optional) max level of RC(k).
+%                   (by default k-level quals the maximal degree of CIJ)
+%
+%   Output:
+%       Rw:         rich-club curve
+%
+%
+%   References:
+%       T Opsahl et al. Phys Rev Lett, 2008, 101(16)
+%       M van den Heuvel, O Sporns, J Neurosci 2011 31(44)
+%
+%   Martijn van den Heuvel, University Medical Center Utrecht, 2011
 
-% [Rw] = rich_club_wd(CIJ,varargin) % rich club curve for weighted graph
-%
-%
-% inputs:
-%               CIJ:       weighted directed connection matrix, but only
-%               total degree matters.
-%
-%               optional:
-%               k-level: max level of RC(k).
-%               When k-level is not given, k-level will be set to max of degree of CIJ
-%                
-% output:
-%               Rw:         rich-club curve
-%
-% adopted from Opsahl et al. Phys Rev Lett, 2008, 101(16)
-%  
-% Martijn van den Heuvel, University Medical Center Utrecht, 2011
-%
-% For details see 'Rich club organization of the human connectome', Martijn van den Heuvel
-% and Olaf Sporns, J Neuroscience 2011 31(44)
-%
-% =========================================================================
+%   Modification History:
+%   2011: Original
+%   2015: Expanded documentation (Mika Rubinov)
 
-NofNodes = size(CIJ,2); %number of nodes
+
+NofNodes = size(CIJ,2); %#ok<NASGU>        %number of nodes
 NodeDegree = sum((CIJ~=0))+sum((CIJ'~=0)); %define degree of each node (indegree + outdegree)
 
 %define to which level rc should be computed
 if size(varargin,2)==1
     klevel = varargin{1};
 elseif isempty(varargin)
-   klevel = max(NodeDegree);   
+    klevel = max(NodeDegree);
 else
     error('number of inputs incorrect. Should be [CIJ], or [CIJ, klevel]')
 end
@@ -39,14 +44,14 @@ end
 %wrank contains the ranked weights of the network, with strongest connections on top
 
 wrank = sort(CIJ(:), 'descend');
-    
-%loop over all possible k-levels 
+
+%loop over all possible k-levels
 for kk = 1:klevel
-
+    
     SmallNodes=find(NodeDegree<kk);
-
-    if isempty(SmallNodes);
-        Rw(kk)=NaN;
+    
+    if isempty(SmallNodes)
+        Rw(kk)=NaN;         %#ok<*AGROW>
         continue
     end
     
@@ -54,25 +59,17 @@ for kk = 1:klevel
     CutoutCIJ=CIJ;
     CutoutCIJ(SmallNodes,:)=[];
     CutoutCIJ(:,SmallNodes)=[];
-
+    
     %total weight of connections in subset E>r
     Wr = sum(CutoutCIJ(:));
-
+    
     %total number of connections in subset E>r
     Er = length(find(CutoutCIJ~=0));
-
+    
     %E>r number of connections with max weight in network
     wrank_r = wrank(1:1:Er);
-
+    
     %weighted rich-club coefficient
     Rw(kk)=Wr / sum(wrank_r);
-
+    
 end
-
-end
- 
-    
-    
-    
-    
-        
