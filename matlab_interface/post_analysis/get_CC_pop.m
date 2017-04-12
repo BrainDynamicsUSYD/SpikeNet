9ba119d3424337055430b7afe1d9f43704f41f5c
Yifan Gu <yigu8115@gmail.com>
2017-04-12 18:16:52 +1000

    Record the indices of the sampled neuron pairs
    Former-commit-id: 0f3560735745a65a3f6c714c0fa77f7986d558ee

--- matlab_interface/post_analysis/get_CC_pop.m
+++ matlab_interface/post_analysis/get_CC_pop.m
@@ -18,7 +18,7 @@ CC_kernel = spike_train_kernel_YG(CC_kernel_width, dt, kernel_type);
 
 % corrcoef within each population
 CC_pop = cell(Num_pop,1);
-
+CC_pop_pairs = cell(Num_pop,1);
 
 if nargin == 1
     pops = 1:Num_pop;
@@ -28,7 +28,7 @@ for pop_ind = pops
     if nnz(num_spikes{pop_ind}) > 0
 
         pairs = rand_unique_pairs( N(pop_ind), corrcoef_sample_num );
-        
+        CC_pop_pairs{pop_ind} = pairs;
         % Calculate pair-wise corrcoef
         for i = 1:corrcoef_sample_num
             CC_pop{pop_ind}(1,i) = CorrCoefYG(spike_hist{pop_ind}(pairs(1,i),:), spike_hist{pop_ind}(pairs(2,i),:), CC_kernel);
@@ -41,12 +41,15 @@ for pop_ind = pops
 %                 fprintf('%10g',i);
 %             end
         end
-        % Record results
-        R.Analysis.CC_pop_kernel_type = kernel_type;
-        R.Analysis.CC_pop_kernel_width = CC_kernel_width;
-        R.Analysis.CC_pop = CC_pop;
+     
     end
 end
+
+% Record results
+R.Analysis.CC_pop_kernel_type = kernel_type;
+R.Analysis.CC_pop_kernel_width = CC_kernel_width;
+R.Analysis.CC_pop = CC_pop;
+R.Analysis.CC_pop_pairs = CC_pop_pairs;
 
 end
 
