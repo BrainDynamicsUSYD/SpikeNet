ade588c82940c957ad51d443c17e67702b4d14be
Yifan Gu <yigu8115@gmail.com>
2017-07-27 13:28:47 +1000

    Remove append mode to allow overwriting with new values
    Former-commit-id: 77199df1d1b79ea66d530d3b43bcfdf4b488d951

--- matlab_interface/write2HDF5/writeBasicParaHDF5.m
+++ matlab_interface/write2HDF5/writeBasicParaHDF5.m
@@ -6,9 +6,9 @@ function writeBasicParaHDF5(FID, dt, step_tot, N)
 %        N: vector for number of neurons in each population
 
 
-hdf5write(FID,'/config/Net/INIT001/N', N, 'WriteMode', 'append');
-hdf5write(FID,'/config/Net/INIT002/dt',dt , 'WriteMode', 'append');
-hdf5write(FID,'/config/Net/INIT002/step_tot', step_tot, 'WriteMode', 'append');
+hdf5write(FID,'/config/Net/INIT001/N', N) %, 'WriteMode', 'append');
+hdf5write(FID,'/config/Net/INIT002/dt',dt) % , 'WriteMode', 'append');
+hdf5write(FID,'/config/Net/INIT002/step_tot', step_tot) %, 'WriteMode', 'append');
 
 % h5create(FID,'/config/Net/INIT001/N', size(N));
 % h5write(FID,'/config/Net/INIT001/N', N);
