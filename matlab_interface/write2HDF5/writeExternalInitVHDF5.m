636d7aab3cd2b83e563734080d3f1efd71e42c74
Yifan Gu <yigu8115@gmail.com>
2017-07-27 13:29:31 +1000

    Fix Guozhang's spelling mistakes
    Former-commit-id: 0d046c0d5daa5df46050d4537214e736534a65da

--- matlab_interface/write2HDF5/writeExternalInitVHDF5.m
+++ matlab_interface/write2HDF5/writeExternalInitVHDF5.m
@@ -1,11 +1,13 @@
-function writeExternalInitVHDF5(FID,pop,exteral_init_V)
+function writeExternalInitVHDF5(FID,pop,external_init_V)
 % write initial condition for membrane potential 
 %            FID: file id for writing data
 %            pop: is the number of population
-% exteral_init_V: is the vector of initial membrane potential for all neurons
+% external_init_V: is the vector of initial membrane potential for all neurons
 %                 of this population
 
-hdf5write(FID,['/config/pops/pop',num2str(pop-1),'/SETINITV/exteral_init_V'],exteral_init_V,'WriteMode','append');   
+hdf5write(FID,['/config/pops/pop',num2str(pop-1),'/SETINITV/external_init_V'],external_init_V,'WriteMode','append');   
+
+end
 
 
     
